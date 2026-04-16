551904b63771096b00d8a06613046a4b2f464790
isaiah-uww <swanson.isaiah@icloud.com>
2026-04-16 14:58:07 -0500

    Update main.js

--- website/js/main.js
+++ website/js/main.js
@@ -1,3 +1,17 @@
+const PREPEND_FILE_PATH = './js/';
+const APPEND_FILE_PATH = '.js';
 document.addEventListener('DOMContentLoaded', (e) => {
   console.log('dom loaded');
+
+  console.log('loading sketch js file');
+  loadScript('lab_1');
 });
+/**
+  dyanmically load script
+*/
+const loadScript = (fileName) => {
+  const src = PREPEND_FILE_PATH + fileName + APPEND_FILE_PATH;
+  const script = document.createElement('script');
+  script.src = src;
+  document.head.append(script);
+};
